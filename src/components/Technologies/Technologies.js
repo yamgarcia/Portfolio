8206ce2e9810d198a65e5f78b6ff9db68b5fb16d
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-02-01 20:43:50 -0800

    Add technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <>
+    <Section id='tech'>
+      <SectionDivider />
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>Stack MERN Java EE + React</SectionText>
+      <List>
+        <ListItem>
+          <DiReact size='3rem' />
+          <ListContainer>
+            <ListTitle>Front End</ListTitle>
+            <ListParagraph>React.js</ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiFirebase size='3rem' />
+          <ListContainer>
+            <ListTitle>Back End</ListTitle>
+            <ListParagraph>Node and Databases</ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiZend size='3rem' />
+          <ListContainer>
+            <ListTitle>UI/UX</ListTitle>
+            <ListParagraph>Figma</ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
+  </>
 );
 
 export default Technologies;
