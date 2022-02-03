b0183ed3b2c563226d495837c79a8d9358088cd3
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-02-02 21:42:47 -0800

    Add acomplishments section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterWrapper>
+        <LinkList>
+          <LinkColumn>
+            <LinkTitle>Call</LinkTitle>
+            <LinkItem href='tell:778-323-2829'>+1 778 323 2829</LinkItem>
+          </LinkColumn>
+          <LinkColumn>
+            <LinkTitle>Email</LinkTitle>
+            <LinkItem href='mailto:yamgarcia.ca@gmail.com'>
+              yamgarcia.ca@gmail.com
+            </LinkItem>
+          </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+          <CompanyContainer>
+            <Slogan> Vancouver - BC </Slogan>
+          </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href='https://github.com'>
+              <AiFillGithub size='3rem' />
+            </SocialIcons>
+
+            <SocialIcons href='https://linkedin.com'>
+              <AiFillLinkedin size='3rem' />
+            </SocialIcons>
+
+            <SocialIcons href='https://instagram.com'>
+              <AiFillInstagram size='3rem' />
+            </SocialIcons>
+          </SocialContainer>
+        </SocialIconsContainer>
+      </FooterWrapper>
+    </>
+  );
+};
+
+export default Footer;
