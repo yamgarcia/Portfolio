0786292979c31c7f778ba71ffa76d945d84884d5
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2023-02-11 15:24:47 -0800

    Change basePath to empty String | New config in next doc

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
+let basePath = ''
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
